6b664d895e545517e4b90f35e465ef90cbc7e718
djibrilsepriramadhan-source <djibrilsepriramadhan@gmail.com>
2026-04-12 22:39:29 +0700

    Add files via upload

---
+++ exercises_on_algebra_data_structures.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+testt_np = np.array([[23], [2], [-5], [73]])
+print(testt_np)
+
+print()
+
+# transporse dari testt_np menjadi testt_t
+testt_t = testt_np.T
+print(f"Hasil Transpose dari testt_np adalah : {testt_t}")
